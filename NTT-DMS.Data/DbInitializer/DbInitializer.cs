c27e69481f041c8835ed0bda4125f9be92b8a2f8
MIIBarisDavran <134509072+MIIBarisDavran@users.noreply.github.com>
2024-07-02 15:48:46 +0300

    db initializer added

---
+++ NTT-DMS.Data/DbInitializer/DbInitializer.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTT_DMS.Data.DbInitializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly DMSContext _context;
+        public DbInitializer(DMSContext db)
+        {
+            _context = db;
+        }
+
+        public void Initialize()
+        {
+
+            try
+            {
+                if(_context.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _context.Database.Migrate();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+    }
+}
